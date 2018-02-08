05614e883a28a1377d1566a02e24f7c5e0883dce
Thomas Denney <me@thomasdenney.co.uk>
2018-02-08 15:19:24 +0000

    Minor fix to rot

--- session3/exercise10.py
+++ session3/exercise10.py
@@ -6,7 +6,7 @@ def test(test_case_xs, test_case_n, expected):
     if actual == expected:
         print("Passed test for " + str(test_case_xs) + ", " + str(test_case_n))
     else:
-        print("Didn't pass test for " + str(test_case) + ", " + str(test_case_n))
+        print("Didn't pass test for " + str(test_case_xs) + ", " + str(test_case_n))
         print("The result was " + str(actual) + " but it should have been " + str(expected))
 
 test([], 0, [])
