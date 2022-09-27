935b496d0d216c59d7d55d73ec9c95edbcfc655b
Oscar Mui <oscarmui1127@gmail.com>
2022-09-27 12:10:15 +0800

    cleaned session 1 materials

---
+++ session1/Example 2: Celsius to Fahrenheit.py
@@ -0,0 +1,5 @@
+# °F = 9/5 * °C + 32
+
+c = float(input("Input temperature in Celsius: "))
+f = 9 / 5 * c + 32
+print(str(c) + " Celsius = " + str(f) + " Fahrenheit")
--- session1/concat.py
+++
@@ -1,7 +0,0 @@
-name = "Thomas"
-age = 19
-
-# If you uncomment the following line of code then it won't run
-# print(name + " is " + age + " years old")
-# You need to do this instead to concatenate (join) strings and numbers:
-print(name + " is " + str(age) + " years old")
